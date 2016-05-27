608b42484e23ad55b1ef5b9044a45bad27418611
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-05-27 20:46:36 +0300

    PyUAVCAN version bump

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ args = dict(
     ],
     install_requires=[
         'setuptools>=18.5',
-        'uavcan>=1.0.0.dev11',
+        'uavcan>=1.0.0.dev12',
         'pyserial>=2.6',
         'qtawesome>=0.3.1',
         'qtconsole>=4.2.0',
