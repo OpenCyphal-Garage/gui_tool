fabd94899116d3fc051df413d405557392862f7b
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-09-25 19:13:29 +0300

    ESC panel implementation

--- uavcan_gui_tool/panels/esc_panel.py
+++ uavcan_gui_tool/panels/esc_panel.py
@@ -8,10 +8,11 @@
 
 import uavcan
 from functools import partial
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel, QDialog, QSlider, QLineEdit
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel, QDialog, QSlider, QSpinBox, QDoubleSpinBox, \
+    QPlainTextEdit
 from PyQt5.QtCore import QTimer, Qt
 from logging import getLogger
-from ..widgets import make_icon_button, get_icon
+from ..widgets import make_icon_button, get_icon, get_monospace_font
 
 __all__ = 'PANEL_NAME', 'spawn', 'get_icon'
 
@@ -28,36 +29,134 @@ class PercentSlider(QWidget):
         super(PercentSlider, self).__init__(parent)
 
         self._slider = QSlider(Qt.Vertical, self)
-        self._slider.setTickInterval(1)
         self._slider.setMinimum(-100)
         self._slider.setMaximum(100)
+        self._slider.setTickInterval(100)
+        self._slider.setTickPosition(QSlider.TicksBothSides)
 
-        self._zero_button = make_icon_button('hand-stop-o', 'Zero setpoint', self,
-                                             on_clicked=lambda: self._slider.setValue(0))
-
-        self._display = QLineEdit(self)
-        self._display.setReadOnly(True)
+        self._zero_button = make_icon_button('hand-stop-o', 'Zero setpoint', self, on_clicked=self.zero)
 
         layout = QVBoxLayout(self)
-        layout.addWidget(self._slider)
+        sub_layout = QHBoxLayout(self)
+        sub_layout.addStretch()
+        sub_layout.addWidget(self._slider)
+        sub_layout.addStretch()
+        layout.addLayout(sub_layout)
         layout.addWidget(self._zero_button)
-        layout.addWidget(self._display)
         self.setLayout(layout)
 
-    def get_value(self):
-        pass
+        self.setMinimumHeight(300)
 
-    def slider_moved(self):
-        pass
+    def zero(self):
+        self._slider.setValue(0)
+
+    def get_value(self):
+        return self._slider.value()
 
 
 class ESCPanel(QDialog):
+    CMD_BIT_LENGTH = uavcan.get_uavcan_data_type(uavcan.equipment.esc.RawCommand().cmd).value_type.bitlen
+    CMD_MAX = 2 ** (CMD_BIT_LENGTH - 1) - 1
+    CMD_MIN = -(2 ** (CMD_BIT_LENGTH - 1))
+
     def __init__(self, parent, node):
         super(ESCPanel, self).__init__(parent)
         self.setWindowTitle('ESC Management Panel')
         self.setAttribute(Qt.WA_DeleteOnClose)              # This is required to stop background timers!
 
         self._node = node
+
+        self._bcast_timer = QTimer(self)
+        self._bcast_timer.start(100)
+        self._bcast_timer.timeout.connect(self._do_broadcast)
+
+        self._sliders = [PercentSlider(self) for _ in range(8)]
+
+        self._num_sliders = QSpinBox(self)
+        self._num_sliders.setMinimum(len(self._sliders))
+        self._num_sliders.setMaximum(20)
+        self._num_sliders.setValue(len(self._sliders))
+        self._num_sliders.valueChanged.connect(self._update_number_of_sliders)
+
+        self._bcast_interval = QDoubleSpinBox(self)
+        self._bcast_interval.setMinimum(0.01)
+        self._bcast_interval.setMaximum(1.0)
+        self._bcast_interval.setSingleStep(0.1)
+        self._bcast_interval.setValue(self._bcast_timer.interval() * 1e-3)
+        self._bcast_interval.valueChanged.connect(
+            lambda: self._bcast_timer.setInterval(self._bcast_interval.value() * 1e3))
+
+        self._stop_all = make_icon_button('hand-stop-o', 'Zero all channels', self, text='Stop All',
+                                          on_clicked=self._do_stop_all)
+
+        self._msg_viewer = QPlainTextEdit(self)
+        self._msg_viewer.setReadOnly(True)
+        self._msg_viewer.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self._msg_viewer.setFont(get_monospace_font())
+        self._msg_viewer.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self._msg_viewer.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+        layout = QVBoxLayout(self)
+
+        self._slider_layout = QHBoxLayout(self)
+        for sl in self._sliders:
+            self._slider_layout.addWidget(sl)
+        layout.addLayout(self._slider_layout)
+
+        layout.addWidget(self._stop_all)
+
+        controls_layout = QHBoxLayout(self)
+        controls_layout.addWidget(QLabel('Channels:', self))
+        controls_layout.addWidget(self._num_sliders)
+        controls_layout.addWidget(QLabel('Broadcast interval:', self))
+        controls_layout.addWidget(self._bcast_interval)
+        controls_layout.addWidget(QLabel('sec', self))
+        controls_layout.addStretch()
+        layout.addLayout(controls_layout)
+
+        layout.addWidget(QLabel('Generated message:', self))
+        layout.addWidget(self._msg_viewer)
+
+        self.setLayout(layout)
+
+    def _do_broadcast(self):
+        try:
+            msg = uavcan.equipment.esc.RawCommand()
+            for sl in self._sliders:
+                raw_value = sl.get_value() / 100
+                value = (-self.CMD_MIN if raw_value < 0 else self.CMD_MAX) * raw_value
+                msg.cmd.append(int(value))
+
+            self._node.broadcast(msg)
+            self._msg_viewer.setPlainText(uavcan.to_yaml(msg))
+        except Exception as ex:
+            self._msg_viewer.setPlainText('Publishing failed:\n' + str(ex))
+
+    def _do_stop_all(self):
+        for sl in self._sliders:
+            sl.zero()
+
+    def _update_number_of_sliders(self):
+        num_sliders = self._num_sliders.value()
+
+        while len(self._sliders) > num_sliders:
+            removee = self._sliders[-1]
+            self._sliders = self._sliders[:-1]
+            self._slider_layout.removeWidget(removee)
+            removee.close()
+            removee.deleteLater()
+
+        while len(self._sliders) < num_sliders:
+            new = PercentSlider(self)
+            self._slider_layout.addWidget(new)
+            self._sliders.append(new)
+
+        def deferred_resize():
+            self.resize(self.minimumWidth(), self.height())
+
+        deferred_resize()
+        # noinspection PyCallByClass,PyTypeChecker
+        QTimer.singleShot(200, deferred_resize)
 
     def __del__(self):
         global _singleton
