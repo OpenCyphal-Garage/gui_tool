cf4d7f94385008af2dd527bc4a7962e32dbf5130
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-09-25 15:50:09 +0300

    Intermediate

--- uavcan_gui_tool/panels/esc_panel.py
+++ uavcan_gui_tool/panels/esc_panel.py
@@ -8,11 +8,12 @@
 
 import uavcan
 from functools import partial
-from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QDialog
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel, QDialog, QSlider, QLineEdit
 from PyQt5.QtCore import QTimer, Qt
 from logging import getLogger
 from ..widgets import make_icon_button, get_icon
 
+__all__ = 'PANEL_NAME', 'spawn', 'get_icon'
 
 PANEL_NAME = 'ESC Panel'
 
@@ -20,6 +21,34 @@ PANEL_NAME = 'ESC Panel'
 logger = getLogger(__name__)
 
 _singleton = None
+
+
+class PercentSlider(QWidget):
+    def __init__(self, parent):
+        super(PercentSlider, self).__init__(parent)
+
+        self._slider = QSlider(Qt.Vertical, self)
+        self._slider.setTickInterval(1)
+        self._slider.setMinimum(-100)
+        self._slider.setMaximum(100)
+
+        self._zero_button = make_icon_button('hand-stop-o', 'Zero setpoint', self,
+                                             on_clicked=lambda: self._slider.setValue(0))
+
+        self._display = QLineEdit(self)
+        self._display.setReadOnly(True)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self._slider)
+        layout.addWidget(self._zero_button)
+        layout.addWidget(self._display)
+        self.setLayout(layout)
+
+    def get_value(self):
+        pass
+
+    def slider_moved(self):
+        pass
 
 
 class ESCPanel(QDialog):
